a20ce8bdec65238fc45b7a657f6779bf6f220e8f
Fizxxc <fizzx909@gmail.com>
2025-06-09 08:11:52 +0700

    Delete public/saveCode.js

--- public/saveCode.js
+++
@@ -1,7 +0,0 @@
-// public/saveCode.js
-import { ref, set } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-database.js";
-import { db } from "./firebase.js";
-
-export async function saveCode(code) {
-  await set(ref(db, `codes/${code}`), true);
-}
